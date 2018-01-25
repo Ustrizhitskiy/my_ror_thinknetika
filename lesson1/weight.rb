fb169ca64f6b60cd6e9b51425f0ecc47b8114f22
Ustrizhitskiy <ustr_serg@mail.ru>
2018-01-25 16:06:11 +0300

    correction lesson1

--- lesson1/weight.rb
+++ lesson1/weight.rb
@@ -1,23 +1,23 @@
-print "Введите Ваш имя: "
+print 'Введите Ваш имя: '
 your_name = gets.chomp
 if (your_name.length < 1)
-	print "Имя должно состоять хотя бы из одного символа. Попробуйте еще: "
-	your_name = gets.chomp
+  print 'Имя должно состоять хотя бы из одного символа. Попробуйте еще: '
+  your_name = gets.chomp
 end
 
-print "Введите Ваш рост в см: "
+print 'Введите Ваш рост в см: '
 growth = gets.chomp.to_i
 if (growth < 1)
-	puts "Рост есть у каждого :) Попробуйте ввести снова: "
-	growth = gets.chomp.to_i
+  puts 'Рост есть у каждого :) Попробуйте ввести снова: '
+  growth = gets.chomp.to_i
 elsif (growth > 1 && growth <= 30)
-	puts "У Вас не может быть такой рост :) Попробуйте ввести снова: "
-	growth = gets.chomp.to_i
+  puts 'У Вас не может быть такой рост :) Попробуйте ввести снова: '
+  growth = gets.chomp.to_i
 end
 
 perfect_weight = growth - 110
-if (perfect_weight < 0)
-	puts "#{your_name}, Ваш вес уже оптимальный"
+if (perfect_weight =< 0)
+  puts "#{your_name}, Ваш вес уже оптимальный"
 else 
-	puts "#{your_name}, Ваш идеальный вес #{perfect_weight}"
+  puts "#{your_name}, Ваш идеальный вес #{perfect_weight}"
 end
