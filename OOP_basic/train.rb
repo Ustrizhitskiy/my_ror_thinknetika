daf7b57b0d179ec61803d8117c07421c87c93e2f
Ustrizhitskiy <ustr_serg@mail.ru>
2018-02-03 10:35:18 +0300

    correction train.rb

--- OOP_basic/train.rb
+++ OOP_basic/train.rb
@@ -1,23 +1,17 @@
 class Train
 
-  attr_reader :number, :type, :quantity_of_wagons, :speed, :train_route
+  attr_reader :number, :type, :quantity_of_wagons, :train_route
+  attr_writer :speed
 
   def initialize(number, type, quantity_of_wagons)
     @number = number
     @type = type
     @quantity_of_wagons = quantity_of_wagons
     @speed = 0
-    @info = [number,type, quantity_of_wagons, speed]
   end
 
   def stop
     self.speed = 0
-  end
-
-  def speed=(speed)
-    @speed = speed
-    @info.delete_at(3)
-    @info.insert(3, @speed)
   end
 
   def add_wagon
@@ -30,9 +24,8 @@ class Train
 
   def get_route(route)
     @train_route = route
-    @info << route.route_number
-    @station_index = 0                    # Номер станции из списка станций на маршруте
-    #@train_route.route_stations[0].parking_trains(self)
+    @station_index = 0
+    @train_route.route_stations[0].parking_trains(self)
   end
 
   def current_station
@@ -44,11 +37,11 @@ class Train
   end
 
   def next_station
-    @train_route.route_stations[@station_index + 1] if @train_route && @station_index < @train_route.route_stations[-1]    
+    @train_route.route_stations[@station_index + 1] if @train_route && @station_index < (@train_route.route_stations.size - 1)
   end
 
   def go_to_next_station
-    if @train_route && @station_index < @train_route.route_stations[-1]
+    if @train_route && @station_index < (@train_route.route_stations.size - 1)
       @train_route.route_stations[@station_index].leaving_trains(self)
       @station_index += 1
       @train_route.route_stations[@station_index].parking_trains(self)
@@ -61,10 +54,6 @@ class Train
       @station_index -= 1
       @train_route.route_stations[@station_index].parking_trains(self)
     end
-  end
-
-  def info
-    puts @info
   end
 
 end
